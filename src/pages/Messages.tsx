fd10f54e57707769815b7518e286efe0935641d8
ayushmalpani1712 <ayushmalpani479@gmail.com>
2026-08-09 18:23:20 +0530

    revert: undo messages URL sync change

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -217,6 +217,7 @@ export default function Messages() {
     const convId = searchParams.get('conversation')
     if (convId) {
       setActiveId(convId)
+      setSearchParams({}, { replace: true })
     }
   }, [])
 
@@ -225,15 +226,6 @@ export default function Messages() {
       setActiveId(conversations[0].id)
     }
   }, [conversations, activeId])
-
-  // Sync activeId to URL so the conversation is deep-linkable
-  useEffect(() => {
-    if (activeId) {
-      setSearchParams({ conversation: activeId }, { replace: true })
-    } else {
-      setSearchParams({}, { replace: true })
-    }
-  }, [activeId, setSearchParams])
 
   useEffect(() => {
     bottomRef.current?.scrollIntoView({ behavior: 'smooth' })
